7a93f5378a538b07cc2fe67dcf2dbb051c1f1c36
devee <devee@DESKTOP-93RQOLS>
2023-11-17 11:21:54 +0530

    commit 3

--- src/test/java/com/ecom/pred/demo1.java
+++ src/test/java/com/ecom/pred/demo1.java
@@ -1,5 +1,7 @@
 package com.ecom.pred;
 
 public class demo1 {
-
+	   public static void main(String[] args) {
+		System.out.println("Push the test to git");
+	}
 }
---
+++ src/test/java/com/ecom/pred/demo2.java
@@ -0,0 +1,7 @@
+package com.ecom.pred;
+
+public class demo2 {
+   public static void main(String[] args) {
+	System.out.println();
+}
+}
