9c39838237f127302f4a08a273707088f35554a1
devee <devee@DESKTOP-93RQOLS.mshome.net>
2023-11-17 12:00:33 +0530

    commit updated

--- src/test/java/com/ecom/pred/demo2.java
+++ src/test/java/com/ecom/pred/demo2.java
@@ -3,5 +3,7 @@ package com.ecom.pred;
 public class demo2 {
    public static void main(String[] args) {
 	System.out.println();
+	System.out.println("something");
+	System.out.println("kuch bhi");
 }
 }
